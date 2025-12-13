dffe211700abafa1e215a25394934644ff9093b5
vinodreddyb86 <vinod@hexacluster.ai>
2025-12-13 14:40:12 +0530

    initial commit

--- cmd/main.go
+++ cmd/main.go
@@ -24,32 +24,50 @@ func main() {
 	defer kafkaProducer.Close()
 
 	telemetryUC := usecase.NewTelemetryProcessor(kafkaProducer)
-	statucUC := usecase.NewStatusProcessor(kafkaProducer)
+	statusUC := usecase.NewStatusProcessor(kafkaProducer)
 	commandUC := usecase.NewCommandProcessor(kafkaProducer)
 	telemetryPool := interfaces.NewWorkerPool(
 		"telemetry", 5, 1000, telemetryUC,
 	)
 	statusPool := interfaces.NewWorkerPool(
-		"status", 5, 500, statucUC,
+		"status", 5, 500, statusUC,
 	)
 	commandPool := interfaces.NewWorkerPool(
 		"command", 5, 100, commandUC,
+	)
+	commandExePool := interfaces.NewWorkerPool(
+		"commandExe", 5, 100, commandUC,
+	)
+	groupPool := interfaces.NewWorkerPool(
+		"group", 5, 100, commandUC,
+	)
+	bootPool := interfaces.NewWorkerPool(
+		"boot", 5, 100, commandUC,
 	)
 
 	telemetryPool.Start(ctx, wg)
 	statusPool.Start(ctx, wg)
 	commandPool.Start(ctx, wg)
+	commandExePool.Start(ctx, wg)
+	groupPool.Start(ctx, wg)
+	bootPool.Start(ctx, wg)
 
 	router := interfaces.NewRouter(
 		telemetryPool.Jobs(),
 		statusPool.Jobs(),
 		commandPool.Jobs(),
+		commandExePool.Jobs(),
+		bootPool.Jobs(),
+		groupPool.Jobs(),
 	)
 
 	subs := []string{
+		"$share/boot-group/neevrfc/boot",
+		"$share/telemetry-group/neevrfc/+/+/cmdexe",
 		"$share/telemetry-group/neevrfc/+/+/telemetry",
 		"$share/status-group/neevrfc/+/+/status",
 		"$share/cmd-group/neevrfc/+/+/cmd",
+		"$share/cmd-group/neevrfc/group/+/cmd",
 	}
 
 	client := infrastructure.NewMQTTClient(
--- internal/interfaces/mqtt_router.go
+++ internal/interfaces/mqtt_router.go
@@ -12,27 +12,39 @@ type MQTTRouter struct {
 	telemetry chan<- domain.Message
 	status    chan<- domain.Message
 	command   chan<- domain.Message
+	cmdexe    chan<- domain.Message
+	boot      chan<- domain.Message
+	groupCmd  chan<- domain.Message
 }
 
-func NewRouter(t, s, c chan<- domain.Message) *MQTTRouter {
+func NewRouter(t, s, c, ce, b, gc chan<- domain.Message) *MQTTRouter {
 	return &MQTTRouter{
 		telemetry: t,
 		status:    s,
 		command:   c,
+		cmdexe:    ce,
+		boot:      b,
+		groupCmd:  gc,
 	}
 }
 
 func (r *MQTTRouter) Handler() mqtt.MessageHandler {
 	return func(_ mqtt.Client, msg mqtt.Message) {
 		m := domain.Message{Topic: msg.Topic(), Payload: msg.Payload()}
-
 		switch {
+		case msg.Topic() == "neevrfc/boot":
+
+			r.boot <- m
+		case strings.HasPrefix(msg.Topic(), "neevrfc/group"):
+			r.groupCmd <- m
+		case !strings.HasPrefix(msg.Topic(), "neevrfc/group") && strings.HasSuffix(msg.Topic(), "/cmd"):
+			r.command <- m
 		case strings.HasSuffix(msg.Topic(), "/telemetry"):
 			r.telemetry <- m
 		case strings.HasSuffix(msg.Topic(), "/status"):
 			r.status <- m
-		case strings.HasSuffix(msg.Topic(), "/cmd"):
-			r.command <- m
+		case strings.HasSuffix(msg.Topic(), "/cmdexe"):
+			r.cmdexe <- m
 		}
 	}
 }
